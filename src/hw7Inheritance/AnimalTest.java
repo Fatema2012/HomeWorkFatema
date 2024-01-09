c3acd441d851a09b385ffad72bea89e72c61b370
Fatema2012 <fatemaaktar4001@gmail.com>
2024-01-08 22:34:29 -0500

    Hw8

--- src/hw7Inheritance/AnimalTest.java
+++ src/hw7Inheritance/AnimalTest.java
@@ -3,68 +3,57 @@ package hw7Inheritance;
 public class AnimalTest {
 
 	public static void main(String[] args) {
-		/*
-		 * Use Java comments to show single inheritance, multilevel inheritance, and Hierarchical inheritance by example from the above. Also, as shown by java comments who is the Parent class, and who is the child class for at least one class?
-		 */
-		Animal animal = new Animal();
+
+		Animal animal = new Animal();// Single inheritance.
 		animal.animalInfo();
 		System.out.println("\n---------------------------------\n");
 
-		Birds birds = new Birds();
+		Birds birds = new Birds();// Hierarchical inheritance.
 		birds.birdsInfo();
 		birds.animalInfo();
 		System.out.println("\n---------------------------------\n");
 
-		BullDog bullDog = new BullDog();
+		BullDog bullDog = new BullDog();// Multilevel Inheritance.
 		bullDog.bullDogInfo();
 		bullDog.dogInfo();
 		bullDog.mammalInfo();
 		bullDog.animalInfo();
 		System.out.println("\n---------------------------------\n");
-		
-		Cobra cobra = new Cobra();
+
+		Cobra cobra = new Cobra();// Multilevel Inheritance.
 		cobra.cobraInfo();
 		cobra.snakeInfo();
 		cobra.reptileInfo();
 		cobra.animalInfo();
 		System.out.println("\n-----------------------------------------\n");
-		
-		
-	Dog dog = new Dog();	
-	dog.dogInfo();
-	dog.mammalInfo();
-	dog.animalInfo();
-System.out.println("\n---------------------------------------------------\n");
 
+		Dog dog = new Dog(); // Multilevel Inheritance.
+		dog.dogInfo();
+		dog.mammalInfo();
+		dog.animalInfo();
+		System.out.println("\n---------------------------------------------------\n");
 
-Mammal mammal = new Mammal();
-mammal.mammalInfo();
-mammal.animalInfo();
-System.out.println("\n-----------------------------------------------------\n");
+		Mammal mammal = new Mammal();// Hierarchical Inheritance.
+		mammal.mammalInfo();
+		mammal.animalInfo();
+		System.out.println("\n-----------------------------------------------------\n");
 
+		Reptile reptile = new Reptile();// Hierarchical Inheritance.
+		reptile.reptileInfo();
+		reptile.animalInfo();
+		System.out.println("\n------------------------------------------------------\n");
 
-Reptile reptile = new Reptile();
-reptile.reptileInfo();
-reptile.animalInfo();
-System.out.println("\n------------------------------------------------------\n");
+		Robin robin = new Robin();// Multilevel Inheritance.
+		robin.robinInfo();
+		robin.birdsInfo();
+		robin.animalInfo();
+		System.out.println("\n-----------------------------------------------------------\n");
 
-
-Robin robin = new Robin();
-robin.robinInfo();
-robin.birdsInfo();
-robin.animalInfo();
-System.out.println("\n-----------------------------------------------------------\n");
-
-
-Snake snake = new Snake();
-snake.snakeInfo();
-snake.reptileInfo();
-snake.animalInfo();
-System.out.println("\n----------------------------------------------------------\n");
+		Snake snake = new Snake();// Multilevel Inheritance
+		snake.snakeInfo();
+		snake.reptileInfo();
+		snake.animalInfo();
+		System.out.println("\n----------------------------------------------------------\n");
 	}
 
-
-
-	}
-
-
+}
--- src/hw7Inheritance/Dog.java
+++ src/hw7Inheritance/Dog.java
@@ -1,8 +1,8 @@
 package hw7Inheritance;
 
-public class Dog  extends Mammal{
+public class Dog extends Mammal {
 
-public void dogInfo() {
-	System.out.println("This method is from Dog Class");
-}
+	public void dogInfo() {
+		System.out.println("This method is from Dog Class");
+	}
 }
